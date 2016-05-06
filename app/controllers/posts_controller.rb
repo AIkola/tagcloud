b904881351b56f6af39b9a2347cabef71c846a53
Antton <antton.ikola@gmail.com>
2016-05-06 12:42:47 +0300

    TagCloud V.1

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,33 @@
+class PostsController < ApplicationController
+  def index
+        @posts = Post.all
+  end
+
+  def create
+  @post = Post.new(post_params)
+  respond_to do |format|
+    if @post.save
+      format.js # Will search for create.js.erb
+    else
+      format.html { render root_path }
+    end
+  end
+
+  end
+
+def index
+  if params[:tag]
+    @posts = Post.tagged_with(params[:tag])
+  else
+    @posts = Post.all
+  end
+end
+
+    private
+def post_params
+  params.require(:post).permit(:author, :content, :all_tags)
+end
+
+
+
+end
